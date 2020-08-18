eb69545e93d971a57911dafd6678e200702b1263
Victoria-Moiseienko <teasevic@gmail.com>
2020-08-18 09:29:23 +0300

    add solution

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,64 @@
 package core.stack;
 
 public class MateStack<T> {
-    public void push(T value) {
+    private Entry<T> tail;
+    private int size;
 
+    public void push(T value) {
+        Entry<T> currentEntry = new Entry<>(value, null, null);
+        if (size != 0) {
+            Entry<T> prevEntry = tail;
+            prevEntry.next = currentEntry;
+            currentEntry.prev = prevEntry;
+        }
+        tail = currentEntry;
+        size++;
     }
 
     public T peek() {
-        return null;
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        return tail.value;
     }
 
     public T pop() {
-        return null;
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        Entry<T> entry = tail;
+        unlink(entry);
+        return entry.value;
+    }
+
+    private void unlink(Entry<T> entry) {
+        Entry<T> entryPrevious = entry.prev;
+        if (entryPrevious != null) {
+            entryPrevious.next = entry.next;
+        }
+
+        Entry<T> entryNext = entry.next;
+        if (entryNext != null) {
+            entryNext.prev = entry.prev;
+        } else {
+            tail = entry.prev;
+        }
+        size--;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private static class Entry<T> {
+        T value;
+        Entry<T> next;
+        Entry<T> prev;
+
+        Entry(T value, Entry<T> next, Entry<T> prev) {
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
     }
 }
