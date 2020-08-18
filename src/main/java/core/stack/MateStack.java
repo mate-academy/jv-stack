ce1d170c87e1f54bfd36d8171b13d0fcbbb6902a
Dm_Grt <grytsishin@gmail.com>
2020-08-18 09:23:47 +0300

    Stack implemented

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,45 @@
 package core.stack;
 
 public class MateStack<T> {
-    public void push(T value) {
+    private static int DEFAULT_CAPACITY = 10;
+    private T[] array;
+    private int size;
+    private int capacity;
 
+    public MateStack() {
+        this.array = (T[]) new Object[DEFAULT_CAPACITY];
+        capacity = array.length;
+    }
+
+    public void push(T value) {
+        if (size == capacity - 1) {
+            grow();
+        }
+        array[++size] = value;
     }
 
     public T peek() {
-        return null;
+        if (size > 0) {
+            return array[size];
+        } else {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
     }
 
     public T pop() {
-        return null;
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        return array[size--];
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void grow() {
+        T[] tempArr = array;
+        array = (T[]) new Object[(int) (capacity * 1.5)];
+        System.arraycopy(tempArr, 0, array, 0, tempArr.length);
     }
 }
