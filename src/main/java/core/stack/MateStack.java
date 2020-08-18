40fb8d263f76e24e06b458069ad76ba0aaa0b65d
Andriy <andriy.lagun@gmail.com>
2020-08-18 09:16:11 +0300

    Implemented stack solution

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,37 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.Arrays;
 
+public class MateStack<T> {
+
+    private Object[] arr = new Object[0];
+    private int size = 0;
+
+    public void push(T value) {
+        arr = Arrays.copyOf(arr, ++size);
+        arr[size - 1] = value;
     }
 
     public T peek() {
-        return null;
+        isNotEmpty();
+        return (T) arr[size - 1];
     }
 
     public T pop() {
-        return null;
+        isNotEmpty();
+        T element = (T) arr[size - 1];
+        arr = Arrays.copyOf(arr, --size);
+        return element;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private boolean isNotEmpty() {
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        return true;
     }
 }
