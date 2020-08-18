3ce281ea78611629db5248ea9b8292ea2a90dccb
tkhambir <tatiana.khambir@gmail.com>
2020-08-18 12:09:58 +0300

    stack solution

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,65 @@
 package core.stack;
 
 public class MateStack<T> {
+    private int size;
+    private Node<T> head;
+
+    public MateStack() {
+        this.size = 0;
+        this.head = null;
+    }
+
     public void push(T value) {
+        Node<T> tmp = head;
+
+        if (size == 0) {
+            head = new Node<>(value, null);
+        } else {
+            head = new Node<>(value, tmp);
+        }
+        size++;
 
     }
 
     public T peek() {
-        return null;
+        checkSize();
+        return head.value;
     }
 
     public T pop() {
-        return null;
+        checkSize();
+        Node<T> tmp = head;
+        T result = tmp.value;
+
+        if (size == 1) {
+            tmp = null;
+            size--;
+            return result;
+        }
+        head = tmp.next;
+        tmp = null;
+        size--;
+
+        return result;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void checkSize() {
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+    }
+
+    private static class Node<T> {
+        private T value;
+        private Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
     }
 }
