aad414ba7ef6615329efdec06a49fdd2b6afa919
Vlad Kuzmin <vladkuzmin22@gmail.com>
2020-08-18 09:26:39 +0300

    hw Stack is done

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,52 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.Arrays;
 
+public class MateStack<T> {
+
+    private int elementCount = 0;
+    private Object[] elementData = new Object[elementCount];
+
+    public void push(T value) {
+        if (elementCount == elementData.length) {
+            elementData = Arrays.copyOf(elementData, elementData.length + 1);
+        }
+        elementData[elementCount] = value;
+        elementCount = elementCount + 1;
     }
 
     public T peek() {
-        return null;
+        if (elementCount == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        return (T) elementData[elementCount - 1];
     }
 
     public T pop() {
-        return null;
+        T obj;
+        int len = size();
+        obj = peek();
+        removeElementAt(len - 1);
+        return obj;
     }
 
     public int size() {
-        return 0;
+        return elementCount;
+    }
+
+    private void removeElementAt(int index) {
+        if (index >= elementCount) {
+            throw new ArrayIndexOutOfBoundsException(index + " >= "
+                    + elementCount);
+        } else if (index < 0) {
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
+
+        int j = elementCount - index - 1;
+        if (j > 0) {
+            System.arraycopy(elementData, index + 1, elementData, index, j);
+        }
+        elementCount--;
+        elementData[elementCount] = null;
     }
 }
