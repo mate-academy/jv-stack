c0b69b418ace660c03c6e1c7ae2cfd53245bab41
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-08-18 09:39:45 +0300

    added solution for stack

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,38 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
+import java.util.List;
+import java.util.ArrayList;
 
+public class MateStack<T> {
+    private int size;
+    private List<T> elementData;
+
+    public MateStack() {
+        size = 0;
+        elementData = new ArrayList<>();
+    }
+
+    public void push(T value) {
+        elementData.add(size, value);
+        size++;
     }
 
     public T peek() {
-        return null;
+        if (size == 0)
+            throw new IndexOutOfBoundsException("Stack is empty");
+        return elementData.get(size - 1);
     }
 
     public T pop() {
-        return null;
+        if (size == 0)
+            throw new IndexOutOfBoundsException("Stack is empty");
+        T poppedElement = peek();
+        size--;
+        return poppedElement;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 }
