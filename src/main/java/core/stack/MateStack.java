0198886241f7f24cb7276e7ef8e196cc87adbe78
Nokafan <volodymy.stepura@gmail.com>
2020-08-18 09:31:38 +0300

    Added solution

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,58 @@
 package core.stack;
 
 public class MateStack<T> {
-    public void push(T value) {
+    private int size;
+    private Node<T> first;
 
+    public MateStack() {
+        this.size = size = 0;
+        this.first = null;
+    }
+
+    public void push(T value) {
+        Node<T> newNode = new Node<>(value, null);
+        if (first == null) {
+            first = newNode;
+            size++;
+            return;
+        }
+        newNode.next = first;
+        first = newNode;
+        size++;
     }
 
     public T peek() {
-        return null;
+        if (first == null) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        return first.value;
     }
 
     public T pop() {
-        return null;
+        if (first == null) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        Node<T> result = first;
+        first = first.next;
+        size--;
+        return result.value;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private static class Node<T> {
+        private T value;
+        private Node<T> next;
+        private Node<T> previous;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+
+        }
     }
 }
+
+
