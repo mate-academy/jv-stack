404fdcf6940d881cfa34b9e35bab127ef3b4d1a9
Fedia <fedia.rudiachenko@gmail.com>
2020-08-18 12:43:55 +0300

    added solution(with array)

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,36 @@
 package core.stack;
 
 public class MateStack<T> {
-    public void push(T value) {
+    private int size;
+    private Object[] arrStack = new Object[10];
 
+    public MateStack() {
+    }
+
+    public void push(T value) {
+        arrStack[++size] = value;
     }
 
     public T peek() {
-        return null;
+        isEmpty();
+        T element = (T) arrStack[size];
+        return element;
     }
 
     public T pop() {
-        return null;
+        isEmpty();
+        T element = (T) arrStack[size];
+        size--;
+        return element;
+    }
+
+    public void isEmpty() {
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 }
