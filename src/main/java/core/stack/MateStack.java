7961161786e07d203af80505167f1b714c5b9e66
ROMASHSX <andriiromash@gmail.com>
2020-08-18 09:41:00 +0300

    try with node

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,62 @@
 package core.stack;
 
 public class MateStack<T> {
-    public void push(T value) {
+    private Node<T> topNode;
+    private Node<T> bottomNode;
+    private int size;
 
+    public MateStack() {
+        size = 0;
+    }
+
+    public void push(T value) {
+        if (size == 0) {
+            topNode = new Node<T>(null, value);
+            bottomNode = new Node<T>(null, value);
+        } else {
+            Node<T> currentNode = topNode;
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            bottomNode = new Node<>(null, value);
+            currentNode.next = bottomNode;
+        }
+        size++;
     }
 
     public T peek() {
-        return null;
+        isEmptyStack();
+        return bottomNode.getElement();
     }
 
     public T pop() {
-        return null;
+        isEmptyStack();
+        T popped = bottomNode.getElement();
+        topNode = topNode.next;
+        size--;
+        return popped;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void isEmptyStack() {
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+    }
+
+    private static class Node<T> {
+        Node<T> next;
+        T element;
+
+        public Node(Node<T> next, T element) {
+            this.next = next;
+            this.element = element;
+        }
+        public T getElement() {
+            return element;
+        }
     }
 }
