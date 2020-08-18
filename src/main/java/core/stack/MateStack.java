4465c9f411e70834051554df8579e0d13d01a665
dariiapikul <dariyapikul@gmail.com>
2020-08-18 09:27:08 +0300

    Solution

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,51 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.ArrayList;
 
+public class MateStack<T> {
+
+    private static final int INITIAL_CAPACITY = 10;
+    private T[] data;
+    private int size;
+
+    public MateStack() {
+        data = (T[]) new Object[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    public void push(T value) {
+        resize();
+        data[size] = value;
+        size++;
     }
 
     public T peek() {
-        return null;
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        return data[size - 1];
     }
 
     public T pop() {
-        return null;
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        T value = data[size - 1];
+        data[size - 1] = null;
+        size--;
+        return value;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void resize() {
+        int length = data.length;
+        if (size == length - 1) {
+            T[] temp = (T[]) new Object[length + length >> 1];
+            System.arraycopy(data,0,temp,0,size);
+            data = temp;
+        }
     }
 }
