5df46ed0e4a93d471f1f04feaca9988495215e33
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-08-18 10:08:15 +0300

    Init commit

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,33 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.LinkedList;
 
+public class MateStack<T> {
+    private LinkedList list = new LinkedList();
+
+    public void push(T value) {
+        list.add(value);
     }
 
     public T peek() {
-        return null;
+        check();
+        return (T) list.getLast();
     }
 
     public T pop() {
-        return null;
+        check();
+        T toReturn = (T) list.getLast();
+        list.removeLast();
+        return toReturn;
     }
 
     public int size() {
-        return 0;
+        return list.size();
+    }
+
+    private void check() {
+        if (size() == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
     }
 }
