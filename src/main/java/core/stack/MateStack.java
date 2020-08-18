7d2df83523a9bc059059d2a3760d8434e278056e
Alkey <Sirj777sirj@gmail.com>
2020-08-18 12:12:30 +0300

    Morning task

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,47 @@
 package core.stack;
 
 public class MateStack<T> {
-    public void push(T value) {
 
+    private Entry<T> first;
+    private int size;
+
+    public void push(T value) {
+        Entry<T> newEntry = new Entry<>(value);
+        if (first == null) {
+            first = newEntry;
+        }
+        newEntry.next = first;
+        first = newEntry;
+        ++size;
     }
 
     public T peek() {
-        return null;
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        return first.value;
     }
 
     public T pop() {
-        return null;
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        T element = first.value;
+        first = first.next;
+        --size;
+        return element;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private static class Entry<T> {
+        private T value;
+        private Entry<T> next;
+
+        private Entry(T value) {
+            this.value = value;
+        }
     }
 }
