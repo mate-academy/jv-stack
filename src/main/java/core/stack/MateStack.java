a9d2330ef8fe34bca583fe0ea2eca670498d5195
AndrewPolishchuk <olkftrylk2@gmail.com>
2020-08-18 09:24:24 +0300

    Added solution

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,44 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.List;
 
+public class MateStack<T> {
+    List<T> stackList;
+    private T tail;
+
+    public MateStack() {
+        stackList = new ArrayList<>();
+    }
+
+    public void push(T value) {
+        stackList.add(value);
+        tail = value;
     }
 
     public T peek() {
-        return null;
+        indexCheck();
+        return stackList.get(stackList.size() - 1);
     }
 
     public T pop() {
-        return null;
+        indexCheck();
+        T removed = stackList.get(stackList.size() - 1);
+        stackList.remove(stackList.size() - 1);
+        return removed;
     }
 
     public int size() {
-        return 0;
+        return stackList.size();
+    }
+
+    private void indexCheck() {
+        if (stackList.size() == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        if (tail == null) {
+            throw new EmptyStackException();
+        }
     }
 }
