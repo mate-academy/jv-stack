7b9005ce8e9d80e11eb07a4f809021d7ca43e5f7
Kostian <pyteec.ua@i.ua>
2020-08-18 21:50:41 +0300

    added solution

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,48 @@
 package core.stack;
 
 public class MateStack<T> {
-    public void push(T value) {
+    private Node<T> last;
+    private int size;
 
+    public MateStack() {
+        last = new Node<>(null, null);
+    }
+
+    public void push(T value) {
+        Node<T> node = last;
+        last = new Node<>(value, node);
+        size++;
     }
 
     public T peek() {
-        return null;
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        return last.element;
     }
 
     public T pop() {
-        return null;
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        T removeNode = last.element;
+        last = last.next;
+        size--;
+        return removeNode;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
+
+    private static class Node<T> {
+        private T element;
+        private Node<T> next;
+
+        public Node(T element, Node<T> next) {
+            this.element = element;
+            this.next = next;
+        }
+    }
+
 }
