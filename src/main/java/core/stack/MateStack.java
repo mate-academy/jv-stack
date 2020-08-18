095a54eb60f29cb8132cb3b93d660ea392941e7f
Alexey <gradiviro@gmail.com>
2020-08-18 12:30:00 +0300

    Stack solution with nodes

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,49 @@
 package core.stack;
 
 public class MateStack<T> {
-    public void push(T value) {
+    private Node<T> lastNode;
+    private int size;
 
+    public MateStack() {
+        lastNode = new Node<>(null, null);
+    }
+
+    public void push(T value) {
+        Node<T> lowerNode = lastNode;
+        lastNode = new Node<>(value, lowerNode);
+        size++;
     }
 
     public T peek() {
-        return null;
+        checkTable();
+        return lastNode.value;
     }
 
     public T pop() {
-        return null;
+        checkTable();
+        T element = lastNode.value;
+        lastNode = lastNode.lowerNode;
+        size--;
+        return element;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void checkTable() {
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+    }
+
+    private static class Node<T> {
+        public T value;
+        public Node<T> lowerNode;
+
+        public Node(T value, Node<T> lowerNode) {
+            this.value = value;
+            this.lowerNode = lowerNode;
+        }
     }
 }
