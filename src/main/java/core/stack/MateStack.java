28eab9d182b7cef901740cd4a38dfa4984591d96
Marina <maryna.voloshyna.11@gmail.com>
2020-08-23 13:22:36 +0300

    stack

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,37 @@
 package core.stack;
 
 public class MateStack<T> {
-    public void push(T value) {
+    public static final int DEFAULT_CAPACITY = 10;
+    private Object[] array;
+    private int size;
 
+    public MateStack() {
+        array = (T[]) new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    public void push(T value) {
+        array[size] = value;
+        size++;
     }
 
     public T peek() {
-        return null;
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        return (T) array[size - 1];
     }
 
     public T pop() {
-        return null;
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        T last = (T) array[size - 1];
+        array[--size] = null;
+        return last;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 }
