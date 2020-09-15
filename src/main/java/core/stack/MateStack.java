d0d7ab870eee2e37f268e12a436e0c28b831acb3
YaTut1901 <59802427+YaTut1901@users.noreply.github.com>
2020-09-15 11:45:50 +0300

    stack

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,39 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MateStack<T> {
+    private List<T> t;
+    private int upperElement;
+
+    public MateStack() {
+        t = new ArrayList<>();
+        upperElement = -1;
+    }
+
+    public void push(T value) {
+        t.add(value);
+        upperElement++;
     }
 
     public T peek() {
-        return null;
+        if (upperElement == -1) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        return t.get(upperElement);
     }
 
     public T pop() {
-        return null;
+        if (upperElement == -1) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        T element = t.remove(upperElement);
+        upperElement--;
+        return element;
     }
 
     public int size() {
-        return 0;
+        return t.size();
     }
 }
