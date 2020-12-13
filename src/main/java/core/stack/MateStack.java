49d48f2857bf1c4a5c620af4a81c3185ef5b605c
IvanLuchkin <id.luchkin@gmail.com>
2020-12-13 13:17:29 +0200

    Initial commit

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,48 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private int size;
+    private Node<T> top;
+
+    public MateStack() {
+        this.size = 0;
+    }
+
+    public void push(T value) {
+        top = new Node<>(value, top);
+        size++;
     }
 
     public T peek() {
-        return null;
+        if (size != 0) {
+            return top.value;
+        }
+        throw new EmptyStackException();
     }
 
     public T pop() {
-        return null;
+        if (size != 0) {
+            T value = top.value;
+            top = top.prev;
+            size--;
+            return value;
+        }
+        throw new EmptyStackException();
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private static class Node<T> {
+        private final T value;
+        private final Node<T> prev;
+
+        Node(T value, Node<T> prev) {
+            this.value = value;
+            this.prev = prev;
+        }
     }
 }
