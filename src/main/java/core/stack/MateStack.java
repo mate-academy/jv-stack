ef4bf9b6d9d4ae7c1ed4bb3c6c5312278dd191c0
Yura <yuramitryahin@gmail.com>
2020-12-16 15:30:10 +0200

    Implemented all methods in class MateStack

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,54 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+
+    private Node<T> tail;
+    private int size;
+
+    public void push(T value) {
+        Node<T> newNode;
+        if (tail == null) {
+            newNode = new Node<>(value, null);
+        } else {
+            newNode = new Node<>(value, tail);
+        }
+        tail = newNode;
+        size++;
     }
 
     public T peek() {
-        return null;
+        checkSize();
+        return tail.value;
     }
 
     public T pop() {
-        return null;
+        checkSize();
+        Node<T> removeNode = tail;
+        tail = removeNode.prevNode;
+        size--;
+        return removeNode.value;
+
+    }
+
+    private void checkSize() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private class Node<T> {
+        private T value;
+        private Node<T> prevNode;
+
+        public Node(T value, Node<T> prevNode) {
+            this.value = value;
+            this.prevNode = prevNode;
+        }
     }
 }
