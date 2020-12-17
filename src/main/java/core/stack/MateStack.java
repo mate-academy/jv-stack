ec9821ff830864f409a24a0dad28b254b65cf0fe
Tonitrus <grom4er@gmail.com>
2020-12-17 15:19:42 +0200

    Complete: Fill methods in clas MateStack

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,20 +1,57 @@
 package core.stack;
 
+import java.util.EmptyStackException;
+
 public class MateStack<T> {
+    private int size;
+    private Node<T> tail;
+
+    public MateStack() {
+        tail = new Node<>(null, null);
+    }
 
     public void push(T value) {
+        if (size == 0) {
+            tail.value = value;
+            size++;
+            return;
+        }
+        Node<T> tempNode = new Node<>(tail, value);
+        tail = tempNode;
+        size++;
 
     }
 
     public T peek() {
-        return null;
+        checkSize();
+        return tail.value;
     }
 
     public T pop() {
-        return null;
+        checkSize();
+        T resultValue = peek();
+        tail = tail.prev;
+        size--;
+        return resultValue;
+    }
+
+    private void checkSize() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private static class Node<T> {
+        Node<T> prev;
+        T value;
+
+        public Node(Node<T> prev, T value) {
+            this.prev = prev;
+            this.value = value;
+        }
     }
 }
