663b1b8e1ecad89624cc601e1a4755fc98e8ef50
YuraBondarenko <yuriybondarenko2001@gmail.com>
2020-12-20 12:53:14 +0200

    created stack via array

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,46 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int DEFAULT_STACK_SIZE = 10;
+    private T[] stack;
+    private int size;
+
+    public MateStack() {
+        stack = (T[]) new Object[DEFAULT_STACK_SIZE];
+        size = 0;
+    }
+
+    public void push(T value) {
+        stack[size] = value;
+        size++;
     }
 
     public T peek() {
-        return null;
+        isEmptyStack();
+        return stack[getIndexOfLastElement()];
     }
 
     public T pop() {
-        return null;
+        isEmptyStack();
+        T lastElement = stack[getIndexOfLastElement()];
+        stack[getIndexOfLastElement()] = null;
+        size--;
+        return lastElement;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void isEmptyStack() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+    }
+
+    private int getIndexOfLastElement() {
+        return size - 1;
     }
 }
