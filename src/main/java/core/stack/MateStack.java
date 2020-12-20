6c77395ee93e8595d87b52b10fe6d03a44d2a3a0
Resci <vIsUAl123>
2020-12-20 11:25:02 +0200

    Add simple stack implementation

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,46 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private Node<T> pointer;
+    private int size;
+
+    public void push(T value) {
+        pointer = new Node<>(pointer, value);
+        size++;
     }
 
     public T peek() {
-        return null;
+        checkIsEmpty();
+        return pointer.value;
     }
 
     public T pop() {
-        return null;
+        checkIsEmpty();
+        T value = pointer.value;
+        pointer = pointer.previous;
+        size--;
+        return value;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void checkIsEmpty() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+    }
+
+    private static class Node<T> {
+        private final Node<T> previous;
+        private final T value;
+
+        private Node(Node<T> previous, T value) {
+            this.previous = previous;
+            this.value = value;
+        }
     }
 }
