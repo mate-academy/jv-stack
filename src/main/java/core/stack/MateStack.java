a848642153caee91beb71c44727d32b373da7e15
Roman Saviak <saviak.roman@gmail.com>
2020-12-20 20:28:45 +0200

    Wrote some methods for my Stack implementation

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,51 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    public static final int DEFAULT_CAPACITY = 10;
+    public static final int MULTIPLICATOR = 2;
+
+    private T [] stackArray;
+    private int size;
+
+    public MateStack() {
+        this.stackArray = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public void push(T value) {
+        resize();
+        stackArray[size] = value;
+        size++;
     }
 
     public T peek() {
-        return null;
+        emptyStack();
+        return stackArray[size - 1];
     }
 
     public T pop() {
-        return null;
+        emptyStack();
+        T removedElement = stackArray[size - 1];
+        stackArray[--size] = null;
+        return removedElement;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    public void resize() {
+        if (size == stackArray.length) {
+            T[] newStackArray = (T[]) new Object[stackArray.length * MULTIPLICATOR];
+            System.arraycopy(stackArray, 0, newStackArray, 0, stackArray.length);
+            stackArray = newStackArray;
+        }
+    }
+
+    public void emptyStack() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
     }
 }
