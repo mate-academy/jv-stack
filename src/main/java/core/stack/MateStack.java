e610247273bc1b7f8630c3789d96d8c02f53d3f3
Igor Bochkor <igorbochkor@gmail.com>
2020-12-21 21:07:02 +0200

    added solution stack

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,51 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] array;
+    private int size;
+
+    public MateStack() {
+        array = (T[]) new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    private void grow() {
+        Object[] newArray = new Object[array.length + (array.length >> 1)];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        array = (T[]) newArray;
+    }
+
+    public void push(T value) {
+        if (size == array.length) {
+            grow();
+        }
+        array[size] = value;
+        size++;
     }
 
     public T peek() {
-        return null;
+        checkException();
+        return array[size - 1];
     }
 
     public T pop() {
-        return null;
+        checkException();
+        T element = array[size - 1];
+        System.arraycopy(array, size + 1, array, size - 1, 0);
+        size--;
+        return element;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void checkException() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
     }
 }
