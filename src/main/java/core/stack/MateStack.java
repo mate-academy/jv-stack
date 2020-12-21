4a54dbb53bc913b0ff2541e56e28fe9acd26540c
Fedenko Maksym <fedenko.maxim.a@gmail.com>
2020-12-21 20:40:35 +0200

    added implementation for four main methods, inner class Node and one helper methods

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,81 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private Node<T> head;
+    private int size;
+
+    public void push(T value) {
+        if (head == null) {
+            head = new Node<>(null, value);
+        } else {
+            Node<T> current = head;
+            while (true) {
+                if (current.next == null) {
+                    current.next = new Node<>(null, value);
+                    break;
+                }
+                current = current.next;
+            }
+        }
+        size++;
     }
 
     public T peek() {
-        return null;
+        sizeChecker(size);
+        Node<T> current = head;
+        while (true) {
+            if (current.next == null) {
+                return current.item;
+            }
+            current = current.next;
+        }
     }
 
     public T pop() {
-        return null;
+        sizeChecker(size);
+        T retValue;
+        Node<T> last = head;
+        while (true) {
+            if (size > 2 && last.next.next == null) {
+                retValue = last.next.item;
+                last.next = null;
+                size--;
+                break;
+            } else if (size == 2) {
+                retValue = head.next.item;
+                head.next = null;
+                size--;
+                break;
+            } else if (size == 1) {
+                retValue = head.item;
+                head = null;
+                size--;
+                break;
+            }
+            last = head.next;
+        }
+        return retValue;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void sizeChecker(int size) {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+    }
+
+    private static class Node<T> {
+        Node<T> next;
+        T item;
+
+        public Node(Node<T> next, T value) {
+            this.next = next;
+            this.item = value;
+        }
     }
 }
