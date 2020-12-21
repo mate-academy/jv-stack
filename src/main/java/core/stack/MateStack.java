14d377114b0e09f68b3559b466c5066d2f08f726
Serhii Semenykhin <semyonich@users.noreply.github.com>
2020-12-21 15:56:27 +0200

    MateStack class implemented

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,31 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.List;
 
+public class MateStack<T> {
+    private List<T> innerArray = new ArrayList<>();
+
+    public void push(T value) {
+        innerArray.add(value);
     }
 
     public T peek() {
-        return null;
+        if (size() == 0) {
+            throw new EmptyStackException();
+        }
+        return (innerArray.get(size() - 1));
     }
 
     public T pop() {
-        return null;
+        if (size() == 0) {
+            throw new EmptyStackException();
+        }
+        return innerArray.remove(size() - 1);
     }
 
     public int size() {
-        return 0;
+        return innerArray.size();
     }
 }
