dcb69d17b7f1c91d73ec92f2aa3a849b442dec4b
Ivan Marchenko <51205960+IwalkInSilence@users.noreply.github.com>
2020-12-22 19:11:23 +0200

    implement class MateStack with LL

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,46 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private Node<T> tail;
+    private int size;
+
+    public void push(T value) {
+        tail = new Node<>(tail, value);
+        size++;
     }
 
     public T peek() {
-        return null;
+        checkStackSize();
+        return tail.value;
     }
 
     public T pop() {
-        return null;
+        checkStackSize();
+        Node<T> oldValue = tail;
+        tail = tail.prev;
+        size--;
+        return oldValue.value;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void checkStackSize() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+    }
+
+    private static class Node<T> {
+        private Node<T> prev;
+        private T value;
+
+        public Node(Node<T> prev, T value) {
+            this.prev = prev;
+            this.value = value;
+        }
     }
 }
