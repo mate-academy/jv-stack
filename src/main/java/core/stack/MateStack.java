c17aab81be4d29b517d125a70708252780dcdc96
ilya-brinchuk <ilya.brinchuk@gmail.com>
2020-12-22 16:46:28 +0200

    created Stack implementation

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,53 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int EMPTY_STACK = 0;
+    private int size = 0;
+    private Node<T> last;
+
+    public void push(T value) {
+        Node<T> newNode = new Node<>(last, value);
+        if (newNode.prev == null) {
+            last = newNode;
+        } else {
+            last = newNode;
+        }
+        size++;
     }
 
     public T peek() {
-        return null;
+        isEmpty();
+        return last.item;
     }
 
     public T pop() {
-        return null;
+        isEmpty();
+        Node<T> lastNode = last;
+        last = last.prev;
+        size--;
+        return lastNode.item;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void isEmpty() {
+        if (size == EMPTY_STACK) {
+            throw new EmptyStackException();
+        }
+    }
+
+    private static class Node<T> {
+        private final T item;
+        private final Node<T> prev;
+
+        private Node(Node<T> prev, T value) {
+            this.item = value;
+            this.prev = prev;
+        }
     }
 }
+
