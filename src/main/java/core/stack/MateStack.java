71ab38422d80e112705c984eb7426b06f4445a8c
ivlytvynets <ivlytvynets@users.noreply.github.com>
2020-12-22 17:26:23 +0200

    new constant DEFAULT_CAPACITY, new method copy() edited push(), pop()

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -4,18 +4,19 @@ import java.util.EmptyStackException;
 
 public class MateStack<T> {
     private static final float LOAD_COEFFICIENT = 1.5f;
+    private static final int DEFAULT_CAPACITY = 10;
     private T[] stack;
     private int size;
     private int capacity;
 
     public MateStack() {
         size = 0;
-        capacity = 10;
+        capacity = DEFAULT_CAPACITY;
         stack = (T[]) new Object[capacity];
     }
 
     public void push(T value) {
-        if (size > capacity) {
+        if (size >= capacity) {
             grow();
         }
         stack[size] = value;
@@ -28,6 +29,7 @@ public class MateStack<T> {
 
     public T pop() {
         T lastItem = getLastItem();
+        stack[size - 1] = null;
         size--;
         return lastItem;
     }
@@ -39,10 +41,7 @@ public class MateStack<T> {
     private void grow() {
         capacity *= LOAD_COEFFICIENT;
         T[] newStack = (T[]) new Object[capacity];
-        for (int i = 0; i < size; i++) {
-            newStack[i] = stack[i];
-        }
-        stack = newStack;
+        copyItems(newStack);
     }
 
     private T getLastItem() {
@@ -50,5 +49,12 @@ public class MateStack<T> {
             return stack[size - 1];
         }
         throw new EmptyStackException();
+    }
+
+    private void copyItems(T[] tempStack) {
+        for (int i = 0; i < size; i++) {
+            tempStack[i] = stack[i];
+        }
+        stack = tempStack;
     }
 }
