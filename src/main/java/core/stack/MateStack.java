4eb1367b2fdf67621c780170318200342622fa14
antonpanchishko <PanchishkoMayor@gmail.com>
2020-12-22 22:07:40 +0200

    1) created custom Stack

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,51 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private Node<T> head;
+    private int size;
+
+    public MateStack() {
+        this.size = 0;
+    }
+
+    public void push(T value) {
+        Node<T> newNode = new Node<>(value, head);
+        head = newNode;
+        size++;
     }
 
     public T peek() {
-        return null;
+        checkLength();
+        return head.value;
     }
 
     public T pop() {
-        return null;
+        checkLength();
+        size--;
+        Node<T> removedNode = head;
+        head = head.prev;
+        return removedNode.value;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void checkLength() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+    }
+
+    private static class Node<T> {
+        private T value;
+        private Node<T> prev;
+
+        Node(T value, Node<T> prev) {
+            this.value = value;
+            this.prev = prev;
+        }
     }
 }
