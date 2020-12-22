db2de0b97426b719993643706efc60afe5f16da2
sanek98cv <73754789+sanek98cv@users.noreply.github.com>
2020-12-22 13:32:04 +0200

    Added implementation

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,44 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private Object[] elementData;
+    private int size;
+
+    public MateStack() {
+        elementData = new Object[10];
+    }
+
+    public void push(T value) {
+        elementData[size++] = value;
     }
 
     public T peek() {
-        return null;
+        if (size() == 0) {
+            throw new EmptyStackException();
+        }
+        return (T) elementData[size - 1];
     }
 
     public T pop() {
-        return null;
+        T value = peek();
+        removeElement(size() - 1);
+        return value;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void removeElement(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayIndexOutOfBoundsException("Index incorrect");
+        }
+        if (size - index - 1 > 0) {
+            System.arraycopy(elementData, index + 1,
+                    elementData, index, size - index - 1);
+        }
+        elementData[size--] = null;
     }
 }
