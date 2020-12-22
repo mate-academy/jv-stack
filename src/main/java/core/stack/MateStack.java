f01346a21692031936ce9ae5525d38b06c820626
Alekearl <alexandrhurko@gmail.com>
2020-12-22 23:26:44 +0200

    created all methods in class MateStack

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,48 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int INITIAL_SIZE_CAPACITY = 10;
+    private static final int ADDING_SIZE = 5;
+    private T[] array;
+    private int size;
+
+    public MateStack() {
+        this.array = (T[]) new Object[INITIAL_SIZE_CAPACITY];
+        size = 0;
+    }
+
+    public void push(T value) {
+        if (size == array.length) {
+            array = grow(array);
+        }
+        array[size] = value;
+        size++;
     }
 
     public T peek() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        return array[size - 1];
     }
 
     public T pop() {
-        return null;
+        T value = peek();
+        array[size - 1] = null;
+        size--;
+        return value;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private T[] grow(T[] arrayValues) {
+        T[] temporaryArray = arrayValues;
+        arrayValues = (T[]) new Object[arrayValues.length + ADDING_SIZE];
+        System.arraycopy(temporaryArray, 0, arrayValues, 0, temporaryArray.length);
+        return arrayValues;
     }
 }
