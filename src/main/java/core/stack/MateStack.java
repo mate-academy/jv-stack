4f04cf5861ad4abe9e466e801cf1d120cb15af70
ivlytvynets <ivlytvynets@users.noreply.github.com>
2020-12-22 15:43:40 +0200

    implemented class MateStack

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,52 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private T[] stack;
+    private int size;
+    private int capacity;
+
+    public MateStack() {
+        size = 0;
+        capacity = 10;
+        stack = (T[]) new Object[capacity];
+    }
+
+    public void push(T value) {
+        size++;
+        if (size > capacity) {
+            grow();
+        }
+        stack[size - 1] = value;
     }
 
     public T peek() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        return stack[size - 1];
     }
 
     public T pop() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        T lastItem = stack[size - 1];
+        size--;
+        return lastItem;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void grow() {
+        capacity *= 1.5;
+        T[] newStack = (T[]) new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            newStack[i] = stack[i];
+        }
+        stack = newStack;
     }
 }
