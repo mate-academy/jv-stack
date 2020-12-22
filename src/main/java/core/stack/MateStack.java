277265fe2b12b3e64cec612ba45d4c5241f1296d
Elena Samsonova <elenasamsonova08@gmail.com>
2020-12-23 01:11:04 +0200

    stack implemented

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,64 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private int size;
+    private Node<T> head;
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        Node(T element, Node<T> next) {
+            this.next = next;
+            this.value = element;
+        }
+    }
+
+    public void push(T value) {
+        Node<T> temp = new Node<>(value, null);
+        Node<T> current = head;
+        if (head == null) {
+            head = temp;
+        } else {
+
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = temp;
+        }
+        size++;
     }
 
     public T peek() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        Node<T> current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        return current.value;
     }
 
     public T pop() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        Node<T> current = head;
+        Node<T> prev = head;
+
+        while (current.next != null) {
+            prev = current;
+            current = current.next;
+        }
+        prev.next = null;
+        size--;
+        return current.value;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 }
