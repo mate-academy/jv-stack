f3c4b355eec05ec95906c83f6a037918e4d06650
ShemsheyMaxim <shanhaymax@gmail.com>
2020-12-22 22:15:46 +0200

    renamed field NUMBER_ONE to ONE, added method isEmptyStack()

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -5,7 +5,7 @@ import java.util.EmptyStackException;
 public class MateStack<T> {
     private static final int CAPACITY = 10;
     private static final double COEFFICIENT = 0.5;
-    private static final int NUMBER_ONE = 1;
+    private static final int ONE = 1;
     private T[] array;
     private int size;
 
@@ -15,7 +15,7 @@ public class MateStack<T> {
     }
 
     public void push(T value) {
-        if (size >= array.length) {
+        if (size == array.length) {
             array = resize(array);
         }
         array[size] = value;
@@ -23,19 +23,15 @@ public class MateStack<T> {
     }
 
     public T peek() {
-        if (size == 0) {
-            throw new EmptyStackException();
-        }
-        return array[size - NUMBER_ONE];
+        isEmptyStack();
+        return array[size - ONE];
     }
 
     public T pop() {
-        if (size == 0) {
-            throw new EmptyStackException();
-        }
-        T currentValue = array[size - NUMBER_ONE];
+        isEmptyStack();
+        T currentValue = array[size - ONE];
+        array[size - ONE] = null;
         size--;
-        array[size] = null;
         return currentValue;
     }
 
@@ -47,5 +43,11 @@ public class MateStack<T> {
         T[] newArray = (T[]) new Object[(int) (array.length * COEFFICIENT + array.length)];
         System.arraycopy(array, 0, newArray, 0, array.length);
         return newArray;
+    }
+
+    private void isEmptyStack() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
     }
 }
