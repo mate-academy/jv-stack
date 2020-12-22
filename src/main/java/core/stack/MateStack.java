6e601814be566602fb39867927c040688d803fd2
Likh-Alex <likhachov991@protonmail.com>
2020-12-22 16:53:39 +0200

    Implemented methods : `push`, `peek`, `pop`, `size`

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,48 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] stack;
+    private int size = 0;
+
+    public MateStack() {
+        this.stack = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public void push(T value) {
+        resize();
+        stack[size] = value;
+        size++;
     }
 
     public T peek() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        return stack[size - 1];
     }
 
     public T pop() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        T topElement = stack[size - 1];
+        stack[size - 1] = null;
+        size--;
+        return topElement;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void resize() {
+        if (size == stack.length) {
+            T[] newStack = (T[]) new Object[(stack.length + (stack.length >> 1))];
+            System.arraycopy(stack, 0, newStack, 0, size);
+            stack = newStack;
+        }
     }
 }
