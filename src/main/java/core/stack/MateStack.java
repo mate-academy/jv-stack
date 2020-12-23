cf0d0e1e54b960e3409e5c3846bc50739744aaad
ivlytvynets <ivlytvynets@users.noreply.github.com>
2020-12-23 11:33:39 +0200

    edited grow()

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -38,7 +38,9 @@ public class MateStack<T> {
 
     private void grow() {
         int newLength = (int) (LOAD_COEFFICIENT * stack.length);
-        System.arraycopy(stack, 0, stack, 0, newLength);
+        T[] newStack = (T[]) new Object[newLength];
+        System.arraycopy(stack, 0, newStack, 0, newLength);
+        stack = newStack;
     }
 
     private T getLastItem() {
