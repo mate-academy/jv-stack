c98c9c70c63dd1cbdafb5524dd6cbe10c647a36c
engineer027 <dp270597dma@gmail.com>
2020-12-23 11:42:15 +0200

    added solution

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -3,25 +3,12 @@ package core.stack;
 import java.util.EmptyStackException;
 
 public class MateStack<T> {
-    private Node<T> head;
     private Node<T> tail;
     private int size = 0;
 
-    private static class Node<T> {
-        private T value;
-        private Node<T> prev;
-
-        public Node(T value, Node<T> prev) {
-            this.value = value;
-            this.prev = prev;
-        }
-    }
-
     public void push(T value) {
         Node<T> newNode = new Node<>(value, null);
-        if (size == 0) {
-            head = newNode;
-        } else {
+        if (size != 0) {
             newNode.prev = tail;
         }
         tail = newNode;
@@ -48,6 +35,16 @@ public class MateStack<T> {
     private void checkNull() {
         if (size == 0) {
             throw new EmptyStackException();
+        }
+    }
+
+    private static class Node<T> {
+        private T value;
+        private Node<T> prev;
+
+        private Node(T value, Node<T> prev) {
+            this.value = value;
+            this.prev = prev;
         }
     }
 }
