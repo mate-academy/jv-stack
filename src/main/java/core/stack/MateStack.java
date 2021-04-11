d7c4b7d06a20fc0f55eb8bb072bafaba1e2b229d
Margarita Kislukhina <marharita.kislukhina@gmail.com>
2021-04-12 00:23:21 +0200

    MateStack implementation added

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,55 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private Node<T> element;
+    private int size;
+
+    private class Node<T> {
+        private T item;
+        private Node<T> next;
+
+        public Node(T item, Node<T> next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    public void push(T value) {
+        if (size == 0) {
+            element = new Node<>(value, null);
+        } else {
+            element = new Node<>(value, element);
+        }
+        size++;
     }
 
     public T peek() {
-        return null;
+        isEmpty();
+        return element.item;
     }
 
     public T pop() {
-        return null;
+        isEmpty();
+        Node<T> removed = element;
+        final T result = removed.item;
+        if (removed.next == null) {
+            element = null;
+        } else {
+            element = removed.next;
+        }
+        size--;
+        return result;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    public void isEmpty() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
     }
 }
