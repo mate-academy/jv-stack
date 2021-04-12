67a683fb8768636fbef4461f86b804c6ea113078
Maksim <max.freedman.main@gmail.com>
2021-04-12 21:20:00 +0300

    put Node class upper

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -6,6 +6,16 @@ public class MateStack<T> {
     private Node<T> top;
     private int size;
 
+    private static class Node<T> {
+        private T value;
+        private Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
     public void push(T value) {
         Node<T> newNode = new Node<>(value, top);
         top = newNode;
@@ -32,16 +42,6 @@ public class MateStack<T> {
     private void isEmptyStack() {
         if (top == null) {
             throw new EmptyStackException();
-        }
-    }
-
-    private static class Node<T> {
-        private T value;
-        private Node<T> next;
-
-        public Node(T value, Node<T> next) {
-            this.value = value;
-            this.next = next;
         }
     }
 }
