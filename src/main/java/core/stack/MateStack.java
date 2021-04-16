3cbaae37fcd512979524eece8b1b13bb2c9e4fa1
SakVladyslav <vsak@itsmartflex.com>
2021-04-16 17:37:51 +0300

    Added Stack implementation

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,44 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int MAX_SIZE = 100;
+    private int top;
+    private T[] stack = (T[]) new Object[MAX_SIZE];
+
+    public MateStack() {
+        top = 0;
+    }
+
+    public void push(T value) {
+        if (top >= MAX_SIZE - 1) {
+            throw new EmptyStackException();
+        } else {
+            top++;
+            stack[top] = value;
+        }
     }
 
     public T peek() {
-        return null;
+        if (top <= 0) {
+            throw new EmptyStackException();
+        } else {
+            return stack[top];
+        }
     }
 
     public T pop() {
-        return null;
+        if (top <= 0) {
+            throw new EmptyStackException();
+        } else {
+            T removedValue = stack[top];
+            top--;
+            return removedValue;
+        }
     }
 
     public int size() {
-        return 0;
+        return top;
     }
 }
