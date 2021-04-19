0d31c9d4209a1645779db02a162dd872ed2d401e
unknown <vovazalli@gmail.com>
2021-04-19 13:08:22 +0300

    all set

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -4,50 +4,40 @@ import java.util.EmptyStackException;
 
 public class MateStack<T> {
     private int size;
-    private Node<T> last;
-    private Node<T> first;
+    private Node<T> current;
 
     private static class Node<T> {
         private final T value;
-        private Node<T> next;
+        private Node<T> prev;
 
-        private Node(T value, Node<T> next) {
+        private Node(Node<T> prev, T value) {
             this.value = value;
-            this.next = next;
+            this.prev = prev;
         }
     }
 
     public void push(T value) {
-        Node<T> nextNode = new Node<>(value, null);
-        if (size == 0) {
-            first = last = nextNode;
-        } else {
-            if (first.next == null) {
-                first.next = nextNode;
-            }
-            last.next = nextNode;
-            last = last.next;
+        Node<T> nextNode = new Node<>(null, value);
+        if (size != 0) {
+            nextNode.prev = current;
         }
+        current = nextNode;
         size++;
     }
 
     public T peek() {
-        if (last == null) {
+        if (current == null) {
             throw new EmptyStackException();
         }
-        return last.value;
+        return current.value;
     }
 
     public T pop() {
-        if (last == null) {
+        if (current == null) {
             throw new EmptyStackException();
         }
-        final Node<T> temp = last;
-        Node<T> iterator = first;
-        while (iterator.next != last && last != first) {
-            iterator = iterator.next;
-        }
-        last = iterator;
+        final Node<T> temp = current;
+        current = current.prev;
         size--;
         return temp.value;
     }
