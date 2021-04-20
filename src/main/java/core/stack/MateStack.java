bf2cb2f7ac41fe11047af11e42bb7af4d4a1702d
Mariia Chekmak <mariiachekmak@MacBook-Pro-Mariia.local>
2021-04-20 19:22:01 +0300

    completed task

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,51 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] elementData;
+    private int size;
+
+    public MateStack() {
+        elementData = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public void push(T value) {
+        if (elementData.length == size) {
+            grow();
+        }
+        elementData[size++] = value;
     }
 
     public T peek() {
-        return null;
+        if (empty() == true) {
+            throw new EmptyStackException();
+        }
+        return elementData[size - 1];
     }
 
     public T pop() {
-        return null;
+        if (empty() == true) {
+            throw new EmptyStackException();
+        }
+        T removedElement = peek();
+        elementData[size--] = null;
+        return removedElement;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    public boolean empty() {
+        return size == 0;
+    }
+
+    private void grow() {
+        T[] newElementData = (T[]) new Object[elementData.length
+                                        + elementData.length / 2];
+        System.arraycopy(elementData, 0, newElementData, 0, size);
+        elementData = newElementData;
     }
 }
