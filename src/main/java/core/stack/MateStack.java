8dfe94c5baa850b1960272ad8c00349da51b82af
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-04-20 21:36:47 +0300

    First version. All tests passed.

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,48 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private Node<T> head;
+    private int size;
+
+    public void push(T value) {
+        if (head == null) {
+            head = new Node<>(value, null);
+        }
+        Node<T> oldHead = head;
+        head = new Node<>(value, oldHead);
+        size++;
     }
 
     public T peek() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        return head.elementValue;
     }
 
     public T pop() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        T headValue = head.elementValue;
+        head = head.next;
+        size--;
+        return headValue;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private class Node<T> {
+        private T elementValue;
+        private Node<T> next;
+
+        public Node(T elementValue, Node<T> next) {
+            this.elementValue = elementValue;
+            this.next = next;
+        }
     }
 }
