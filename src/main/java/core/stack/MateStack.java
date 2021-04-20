706adb775096efb3044807b87faf72031ad39b7a
DeStinyShell <dmitriy4ever97@gmail.com>
2021-04-20 21:09:38 +0300

    Done Homework.

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,45 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int DEFAULT_ARRAY_CAPACITY = 10;
+    private int size;
+    private T[] dataArray = (T[]) new Object[DEFAULT_ARRAY_CAPACITY];
+
+    public void push(T value) {
+        grow();
+        dataArray[size++] = value;
     }
 
     public T peek() {
-        return null;
+        checkArrayIsEmpty();
+        return dataArray[size - 1];
     }
 
     public T pop() {
-        return null;
+        checkArrayIsEmpty();
+        T value = dataArray[size - 1];
+        dataArray[size - 1] = null;
+        size--;
+        return value;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void checkArrayIsEmpty() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+    }
+
+    private void grow() {
+        if (size == dataArray.length) {
+            T[] array = (T[]) new Object[size + (size >> 1)];
+            System.arraycopy(dataArray, 0, array, 0, size);
+            dataArray = array;
+        }
     }
 }
