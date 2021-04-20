24f7d66d4ed625c82bf52e9e82d97a1dfcd6e07b
Oksana Lashchenko <lashchenkooksana@gmail.com>
2021-04-20 17:55:25 +0300

    first commit

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,50 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int INITIAL_CAPACITY = 10;
+    private T[] elements;
+    private int size;
+    private int topOfStack;
+
+    public MateStack() {
+        this.elements = (T[]) new Object[INITIAL_CAPACITY];
+        this.size = 0;
+        this.topOfStack = -1;
+    }
+
+    public void push(T value) {
+        if (size == elements.length) {
+            extendArray();
+        }
+        elements[++topOfStack] = value;
+        size++;
     }
 
     public T peek() {
-        return null;
+        if (topOfStack < 0) {
+            throw new EmptyStackException();
+        }
+        return elements[topOfStack];
     }
 
     public T pop() {
-        return null;
+        if (topOfStack < 0) {
+            throw new EmptyStackException();
+        }
+        size--;
+        return elements[topOfStack--];
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void extendArray() {
+        int newCapacity = Math.round((size * 3) / 2 + 1);
+        T[] extendedArray = (T[]) new Object[newCapacity];
+        System.arraycopy(elements, 0, extendedArray, 0, size);
+        elements = extendedArray;
     }
 }
