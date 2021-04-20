a008b2384a5f9db3ecb681b029b68dbcedd68465
redxp <redxpertus1@gmail.com>
2021-04-20 13:38:08 +0300

    Stack v.01

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,61 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private Node<T> top;
+    private int size;
+
+    public static class Node<T> {
+        private Node<T> prev;
+        private T value;
+
+        public Node() {
+        }
+
+        public Node(T value, Node prev) {
+            this.prev = prev;
+            this.value = value;
+        }
+    }
+
+    public void push(T value) {
+        if (size == 0) {
+            Node<T> node = top;
+            top = new Node<>();
+            top.value = value;
+            top.prev = node;
+            size++;
+            return;
+        }
+        Node<T> newNode = new Node<>();
+        newNode.value = value;
+        newNode.prev = top;
+        top = newNode;
+        size++;
     }
 
     public T peek() {
-        return null;
+        checkEmpty();
+        return top.value;
     }
 
     public T pop() {
-        return null;
+        checkEmpty();
+        Node<T> currentNodeValue = new Node<>();
+        currentNodeValue.value = top.value;
+        top = top.prev;
+        size--;
+        return currentNodeValue.value;
     }
 
     public int size() {
-        return 0;
+        return (size == 0) ? 0 : size;
+    }
+
+    private void checkEmpty() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
     }
 }
