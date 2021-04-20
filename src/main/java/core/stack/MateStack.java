2896a518b573ece43cc8e8ddf34990c0fccab667
Anatolii Zarytskyi <tolia.zaritskiy@gmail.com>
2021-04-20 20:39:59 +0300

    added solution

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,44 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] array;
+    private int size;
+
+    public MateStack() {
+        array = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void push(T value) {
+        if (array.length == size) {
+            grow();
+        }
+        array[size++] = value;
     }
 
     public T peek() {
-        return null;
+        int len = size();
+        if (len == 0) {
+            throw new EmptyStackException();
+        }
+        return (T) array[len - 1];
     }
 
     public T pop() {
-        return null;
+        T object = peek();
+        size--;
+        return object;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private T[] grow() {
+        T[] grewArray = (T[]) new Object[size + (size / 2)];
+        System.arraycopy(array, 0, grewArray, 0, size);
+        return grewArray;
     }
 }
