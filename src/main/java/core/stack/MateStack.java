b879c5fc801e90a19e826efe6ce76edae56b07d2
anufriiev <dimaanufriev1012@gmail.com>
2021-04-20 18:47:20 +0300

    Added solution

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,55 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final double SIZE_MULTIPLICATION = 1.5;
+    private int size;
+    private T[] array;
+    private int index;
+
+    public MateStack() {
+        array = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public void push(T value) {
+        if (size == array.length) {
+            reSize();
+        }
+        array[size] = value;
+        size++;
     }
 
     public T peek() {
-        return null;
+        checkEmpty();
+        index = size - 1;
+        return array[index];
     }
 
     public T pop() {
-        return null;
+        checkEmpty();
+        index = size - 1;
+        T returnElement = array[index];
+        array[index] = null;
+        size--;
+        return returnElement;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    public void reSize() {
+        T[] newArray = (T[]) new Object[(int) (array.length * SIZE_MULTIPLICATION)];
+        System.arraycopy(array, 0, newArray, 0, array.length - 1);
+        array = newArray;
+    }
+
+    public boolean checkEmpty() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        return false;
     }
 }
