cc777c0cf9ee9fe58e7b4139281df3f76aab3554
Iryna Ustymenko <i.ustimenko18@gmail.com>
2021-04-20 11:52:15 +0300

    created MateStack

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,52 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private Node<T> bottom;
+    private Node<T> top;
+    private int size;
+
+    private static class Node<T> {
+        private final T value;
+        private Node<T> prev;
+
+        Node(T element, Node top) {
+            this.value = element;
+            this.prev = top;
+        }
+    }
+
+    public void push(T value) {
+        Node<T> node = new Node<>(value, top);
+
+        if (bottom == null) {
+            bottom = node;
+        }
+        top = node;
+        size++;
     }
 
     public T peek() {
-        return null;
+        try {
+            return top.value;
+        } catch (NullPointerException e) {
+            throw new EmptyStackException();
+        }
     }
 
     public T pop() {
-        return null;
+        try {
+            T saveValue = top.value;
+            top = top.prev;
+            size--;
+            return saveValue;
+        } catch (NullPointerException e) {
+            throw new EmptyStackException();
+        }
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 }
