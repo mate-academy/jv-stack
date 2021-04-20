186c3c11cc61e36fbf66440046a6bc96488c8d44
the5tranger <mishaantonyuk00@gmail.com>
2021-04-20 11:46:37 +0300

    Stack Done v1.0

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,49 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] stackArray;
+    private int size;
+
+    public MateStack() {
+        stackArray = (T[]) new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    public void push(T value) {
+        if (size == stackArray.length) {
+            grow();
+        }
+        stackArray[size] = value;
+        size++;
     }
 
     public T peek() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        return stackArray[size - 1];
     }
 
     public T pop() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        T returnValue = stackArray[size - 1];
+        stackArray[size - 1] = null;
+        size--;
+        return returnValue;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void grow() {
+        T[] data = stackArray;
+        stackArray = (T[]) new Object[size * 3 / 2];
+        System.arraycopy(data, 0, stackArray, 0, size);
     }
 }
