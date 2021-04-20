fe3f867c5cf8a3dc32b37be3e403c933e5abf254
Darina <to.darina.to>
2021-04-20 18:19:42 +0300

    test done

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,45 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private Node<T> tail;
+    private int size;
+
+    public void push(T value) {
+        tail = new Node<>(value, tail);
+        size++;
     }
 
     public T peek() {
-        return null;
+        checkSize();
+        return tail.item;
     }
 
     public T pop() {
-        return null;
+       checkSize();
+        Node<T> node = tail;
+        tail = node.prev;
+        size--;
+        return node.item;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void checkSize () {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+    }
+    private static class Node<T> {
+        private final T item;
+        private final Node<T> prev;
+
+        private Node(T value, Node<T> prev) {
+            this.item = value;
+            this.prev = prev;
+        }
     }
 }
