e85041f444cbf3e705d0f72f2e53d5892bb17e29
oleksandr-shulha <shulgalex@gmail.com>
2021-04-20 20:35:07 +0300

    All tests

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,44 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private int size;
+    private Node<T> node;
+
+    private static class Node<T> {
+        private final T value;
+        private Node<T> prev;
+
+        private Node(Node<T> prev, T value) {
+            this.value = value;
+            this.prev = prev;
+        }
+    }
+
+    public void push(T value) {
+        size++;
+        node = new Node<>(node, value);
     }
 
     public T peek() {
-        return null;
+        if (node == null) {
+            throw new EmptyStackException();
+        }
+        return node.value;
     }
 
     public T pop() {
-        return null;
+        if (node == null) {
+            throw new EmptyStackException();
+        }
+        Node<T> temp = node;
+        node = node.prev;
+        size--;
+        return temp.value;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 }
