8ca6da982744a34a577837d586e316269f56adc2
Koziarevskyi <koziarevskyi@gmail.com>
2021-04-20 22:14:05 +0300

    C001 - All done! For review!

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,46 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private Node<T> last;
+    private int size;
+
+    public void push(T value) {
+        last = new Node<>(value, last);
+        size++;
     }
 
     public T peek() {
-        return null;
+        checkSize();
+        return last.item;
     }
 
     public T pop() {
-        return null;
+        checkSize();
+        final T result = last.item;
+        last = last.previous;
+        size--;
+        return result;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private static class Node<T> {
+        private T item;
+        private Node<T> previous;
+
+        public Node(T item, Node<T> previous) {
+            this.item = item;
+            this.previous = previous;
+        }
+    }
+
+    private void checkSize() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
     }
 }
