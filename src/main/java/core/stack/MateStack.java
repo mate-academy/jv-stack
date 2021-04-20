f86087f31ce83f8a280fcf39528b861a640837a5
Aalderaan <chyregd@gmail.com>
2021-04-21 00:14:58 +0200

    solitions to Stack

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,50 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private int size = 0;
+    private T[] elements;
+
+    public MateStack() {
+        elements = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public void push(T value) {
+        if (size == elements.length) {
+            elements = resize();
+        }
+        elements[size] = value;
+        size++;
     }
 
     public T peek() {
-        return null;
+        checkIfIsEmpty();
+        return elements[size - 1];
     }
 
     public T pop() {
-        return null;
+        checkIfIsEmpty();
+        T popElement = elements[size - 1];
+        elements[size - 1] = null;
+        size--;
+        return popElement;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private T[] resize() {
+        T[] extendedArray = (T[]) new Object[(size * 3) / 2];
+        System.arraycopy(elements, 0, extendedArray, 0, size);
+        return extendedArray;
+    }
+
+    private void checkIfIsEmpty() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
     }
 }
