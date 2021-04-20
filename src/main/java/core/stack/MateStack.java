7936e4d22c73cc88027e7916c9263422f45b33f4
Yevhenii Denysenko <iedenysenko@gmail.com>
2021-04-20 13:57:52 +0300

    added solution

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,47 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int ARRAY_SIZE = 10;
+    private static final int FIRST_INDEX = 0;
+    private static final int DECREMENT = 1;
+    private static final double COEFFICIENT_INCREASE_ARRAY = 1.5;
+    private T[] array;
+    private int size;
+
+    public MateStack() {
+        array = (T[]) new Object[ARRAY_SIZE];
+    }
+
+    public void push(T value) {
+        array[size++] = value;
+        if (size == array.length) {
+            growArray();
+        }
     }
 
     public T peek() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        return array[size - DECREMENT];
     }
 
     public T pop() {
-        return null;
+        T removed;
+        removed = peek();
+        array[--size] = null;
+        return removed;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void growArray() {
+        T[] newArray = (T[]) new Object[(int) (array.length * COEFFICIENT_INCREASE_ARRAY)];
+        System.arraycopy(array, FIRST_INDEX, newArray, FIRST_INDEX, size);
+        array = newArray;
     }
 }
