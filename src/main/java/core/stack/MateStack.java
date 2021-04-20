cbb41cac41586841e5675148649387c712d8381d
Den4eg007 <68178197+Den4eg007@users.noreply.github.com>
2021-04-20 13:38:20 +0300

    Initial implementation of Stack

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,50 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private Node<T> tail;
+    private int size;
+
+    public void push(T value) {
+        if (tail == null) {
+            tail = new Node<>(value, null);
+        } else {
+            tail = new Node<>(value, tail);
+        }
+        size++;
     }
 
     public T peek() {
-        return null;
+        checkSize();
+        return tail.value;
     }
 
     public T pop() {
-        return null;
+        checkSize();
+        T element = tail == null ? null : tail.value;
+        tail = tail == null ? null : tail.prev;
+        size--;
+        return element;
+    }
+
+    private void checkSize() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private static class Node<T> {
+        private T value;
+        private Node<T> prev;
+
+        private Node(T value, Node<T> prev) {
+            this.value = value;
+            this.prev = prev;
+        }
     }
 }
