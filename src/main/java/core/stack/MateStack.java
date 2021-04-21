8e36bee9add1ad3e2015d71ef243f4f5949294ed
Kostiantyn <konkos2505@gmail.com>
2021-04-21 14:43:50 +0300

    add new solution with Node class

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -3,34 +3,31 @@ package core.stack;
 import java.util.EmptyStackException;
 
 public class MateStack<T> {
-    private static final int START_ARRAY_CAPACITY = 10;
-    private static final double CAPACITY_ARRAY_MULTIPLE = 1.5;
+    private Node<T> top;
     private int size;
-    private T[] stackArray;
-    private int capacity;
-
-    public MateStack() {
-        capacity = START_ARRAY_CAPACITY;
-        stackArray = (T[]) new Object[capacity];
-    }
 
     public void push(T value) {
-        checkArraySize();
-        stackArray[size] = value;
+        Node<T> node;
+        if (size == 0) {
+            node = new Node<>(value, null);
+        } else {
+            node = new Node<>(value, top);
+        }
+        top = node;
         size++;
     }
 
     public T peek() throws EmptyStackException {
         if (size > 0) {
-            return stackArray[size - 1];
+            return top.value;
         }
         throw new EmptyStackException();
     }
 
     public T pop() throws EmptyStackException {
         if (size > 0) {
-            T value = stackArray[size - 1];
-            stackArray[size - 1] = null;
+            T value = top.value;
+            top = top.prev;
             size--;
             return value;
         }
@@ -41,12 +38,13 @@ public class MateStack<T> {
         return size;
     }
 
-    private void checkArraySize() {
-        if (size == capacity) {
-            int newCapacity = (int) (capacity * CAPACITY_ARRAY_MULTIPLE);
-            T[] newArray = (T[]) new Object[newCapacity];
-            System.arraycopy(stackArray, 0, newArray, 0, stackArray.length);
-            stackArray = newArray;
+    private class Node<T> {
+        private T value;
+        private Node<T> prev;
+
+        public Node(T value, Node<T> prev) {
+            this.value = value;
+            this.prev = prev;
         }
     }
 }
