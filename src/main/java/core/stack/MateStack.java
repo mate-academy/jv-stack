785804fc2b79230d9100dd4eda6a603f46c73b4e
Alina <vasylyshyn.alina@gmail.com>
2021-04-21 13:23:20 +0300

    implemented method puss, peek, pop

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -4,19 +4,10 @@ import java.util.EmptyStackException;
 
 public class MateStack<T> {
     private Node<T> head;
-    private Node<T> tail;
     private int size;
 
     public void push(T value) {
-        if (size == 0) {
-            head = new Node<>(null, value, null);
-            tail = head;
-            size++;
-            return;
-        }
-        Node<T> newNode = new Node<>(tail, value, null);
-        tail.next = newNode;
-        tail = newNode;
+        head = new Node<>(value, head);
         size++;
     }
 
@@ -24,76 +15,30 @@ public class MateStack<T> {
         if (size == 0) {
             throw new EmptyStackException();
         }
-        Node<T> newNode = findNode(size - 1);
-        return newNode.value;
+        return head.value;
     }
 
     public T pop() {
         if (size == 0) {
             throw new EmptyStackException();
         }
-        Node<T> newNode = findNode(size - 1);
-        remove(size - 1);
-        return newNode.value;
+        T lastValue = head.value;
+        head = head.prev;
+        size--;
+        return lastValue;
     }
 
     public int size() {
         return size;
     }
 
-    private Node<T> findNode(int index) {
-        Node<T> newNode;
-        if (index < size) {
-            newNode = head;
-            for (int i = 0; i < index; i++) {
-                newNode = newNode.next;
-            }
-            return newNode;
-        } else {
-            newNode = tail;
-            for (int i = size - 1; i > index; i--) {
-                newNode = newNode.prev;
-            }
-            return newNode;
-        }
-    }
-
-    private T unlink(Node<T> node) {
-        Node<T> prevNode = node.prev;
-        Node<T> nextNode = node.next;
-        if (prevNode == null) {
-            head = nextNode;
-        } else {
-            prevNode.next = nextNode;
-            node.prev = null;
-        }
-        if (nextNode == null) {
-            tail = prevNode;
-        } else {
-            nextNode.prev = prevNode;
-            node.next = null;
-        }
-        T result = node.value;
-        size--;
-        return result;
-    }
-
-    private T remove(int index) {
-        Node<T> newNode = findNode(index);
-        T removeValue = newNode.value;
-        unlink(newNode);
-        return removeValue;
-    }
-
     private class Node<T> {
-        private Node<T> prev;
         private T value;
-        private Node<T> next;
+        private Node<T> prev;
 
-        public Node(Node<T> prev, T value, Node<T> next) {
-            this.prev = prev;
+        public Node(T value, Node<T> next) {
             this.value = value;
-            this.next = next;
+            this.prev = next;
         }
     }
 }
