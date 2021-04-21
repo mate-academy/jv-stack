84e1c391879f25392ea75936afc80eb58ae0d2d7
Iryna Ustymenko <i.ustimenko18@gmail.com>
2021-04-21 07:43:45 +0300

    created method checkSize

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -6,6 +6,29 @@ public class MateStack<T> {
     private Node<T> top;
     private int size;
 
+    public void push(T value) {
+        Node<T> node = new Node<>(value, top);
+        top = node;
+        size++;
+    }
+
+    public T peek() {
+        checkSize();
+        return top.value;
+    }
+
+    public T pop() {
+        checkSize();
+        T saveValue = top.value;
+        top = top.prev;
+        size--;
+        return saveValue;
+    }
+
+    public int size() {
+        return size;
+    }
+
     private static class Node<T> {
         private final T value;
         private Node<T> prev;
@@ -16,32 +39,9 @@ public class MateStack<T> {
         }
     }
 
-    public void push(T value) {
-        Node<T> node = new Node<>(value, top);
-        top = node;
-        size++;
-    }
-
-    public T peek() {
-        try {
-            return top.value;
-        } catch (NullPointerException e) {
+    public void checkSize() {
+        if (size == 0) {
             throw new EmptyStackException();
         }
-    }
-
-    public T pop() {
-        try {
-            T saveValue = top.value;
-            top = top.prev;
-            size--;
-            return saveValue;
-        } catch (NullPointerException e) {
-            throw new EmptyStackException();
-        }
-    }
-
-    public int size() {
-        return size;
     }
 }
