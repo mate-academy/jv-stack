c3f57ebd064852c70783c08a5ec2d0af93dfac30
Jakub <jszymanski97@gmail.com>
2024-04-05 17:28:03 +0100

    First Attempt

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,52 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int DEF_CAPACITY = 10;
+    private Object[] array;
+    private int size;
+
+    public MateStack() {
+        array = new Object[DEF_CAPACITY];
+        size = 0;
+    }
+
+    public void push(T value) {
+        checkCapacity();
+        array[size++] = value;
     }
 
     public T peek() {
-        return null;
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return (T) array[size - 1];
     }
 
     public T pop() {
-        return null;
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        T current = (T) array[--size];
+        array[size] = null;
+        return current;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void checkCapacity() {
+        if (size == array.length) {
+            int newSize = array.length * 2;
+            Object[] newArray = new Object[newSize];
+            System.arraycopy(array, 0, newArray, 0, size);
+            array = newArray;
+        }
     }
 }
