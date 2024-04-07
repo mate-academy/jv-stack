519423270e1464518a814650caa9840b1280268d
Jakub <jszymanski97@gmail.com>
2024-04-07 21:46:42 +0100

    Class type parameter put to use, constant for Grow Factor created, two methods made private, checkNotEmpty method created

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -4,12 +4,12 @@ import java.util.EmptyStackException;
 
 public class MateStack<T> {
     private static final int DEF_CAPACITY = 10;
-    private Object[] array;
+    private static final int GROW_FACTOR = 2;
+    private T[] array;
     private int size;
 
     public MateStack() {
-        array = new Object[DEF_CAPACITY];
-        size = 0;
+        array = (T[]) new Object[DEF_CAPACITY];
     }
 
     public void push(T value) {
@@ -18,17 +18,13 @@ public class MateStack<T> {
     }
 
     public T peek() {
-        if (isEmpty()) {
-            throw new EmptyStackException();
-        }
-        return (T) array[size - 1];
+        checkNotEmpty();
+        return array[size - 1];
     }
 
     public T pop() {
-        if (isEmpty()) {
-            throw new EmptyStackException();
-        }
-        T current = (T) array[--size];
+        checkNotEmpty();
+        T current = array[--size];
         array[size] = null;
         return current;
     }
@@ -37,16 +33,22 @@ public class MateStack<T> {
         return size;
     }
 
-    public boolean isEmpty() {
+    private boolean isEmpty() {
         return size == 0;
     }
 
-    public void checkCapacity() {
+    private void checkCapacity() {
         if (size == array.length) {
-            int newSize = array.length * 2;
-            Object[] newArray = new Object[newSize];
+            int newSize = array.length * GROW_FACTOR;
+            T[] newArray = (T[]) new Object[newSize];
             System.arraycopy(array, 0, newArray, 0, size);
             array = newArray;
+        }
+    }
+
+    private void checkNotEmpty() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
         }
     }
 }
