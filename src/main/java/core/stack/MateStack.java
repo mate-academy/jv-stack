cf4eacb94ab5e8e01a0eeed6eac6af21292be131
mgsorigo <marek.gucma@sorigo.pl>
2024-06-07 12:19:30 +0200

    fix

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -11,9 +11,19 @@ public class MateStack<T> {
         this.size = 0;
     }
 
+    private static class Node<T> {
+        private final T data;
+        private Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
     public void push(T value) {
         Node<T> newNode = new Node<>(value);
-        newNode.setNext(top);
+        newNode.next = top;
         top = newNode;
         size++;
     }
@@ -22,15 +32,15 @@ public class MateStack<T> {
         if (isEmpty()) {
             throw new EmptyStackException();
         }
-        return top.getData();
+        return top.data;
     }
 
     public T pop() {
         if (isEmpty()) {
             throw new EmptyStackException();
         }
-        T value = top.getData();
-        top = top.getNext();
+        T value = top.data;
+        top = top.next;
         size--;
         return value;
     }
