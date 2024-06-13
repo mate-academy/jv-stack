4a5a3e9d561b0071deaebefc7478eb507f0aefa0
Tomasz Baran <MarcusOfficial@tuta.io>
2024-06-13 12:14:37 +0200

    initial solution

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,53 @@
 package core.stack;
 
+import java.util.EmptyStackException;
+
 public class MateStack<T> {
+    private static final int GROWTH_FACTOR = 2;
+    private T[] stack;
+    private int size;
+    private int capacity;
+
+    public MateStack() {
+        final int default_capacity = 10;
+        stack = (T[]) new Object[default_capacity];
+        size = 0;
+        capacity = default_capacity;
+    }
+
     public void push(T value) {
-
+        if (size >= capacity) {
+            grow();
+        }
+        stack[size] = value;
+        size++;
     }
 
-    public T peek() {
-        return null;
+    public T peek() throws EmptyStackException {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        return stack[size - 1];
     }
 
-    public T pop() {
-        return null;
+    public T pop() throws EmptyStackException {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        T poppedElement = stack[size - 1];
+        stack[size - 1] = null;
+        size--;
+        return poppedElement;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void grow() {
+        T[] oldStack = stack;
+        capacity *= GROWTH_FACTOR;
+        stack = (T[]) new Object[capacity];
+        System.arraycopy(oldStack, 0, stack, 0, size);
     }
 }
