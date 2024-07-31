88dbd023aecd1b7bcc307d5c68e9ee6770bb5cbe
Artur <a_blyskal@wp.pl>
2024-07-31 16:31:54 +0200

    jv-stack - implementation

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,53 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private Node<T> top;
+    private int size;
+
+    public MateStack() {
+        this.top = null;
+        this.size = 0;
+    }
+
+    public void push(T value) {
+        top = new Node<>(value, top);
+        size++;
     }
 
     public T peek() {
-        return null;
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return top.value;
     }
 
     public T pop() {
-        return null;
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        T value = top.value;
+        top = top.next;
+        size--;
+        return value;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private boolean isEmpty() {
+        return top == null;
+    }
+
+    private static class Node<T> {
+        private final T value;
+        private final Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
     }
 }
