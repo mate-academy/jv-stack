55f12de3b1bc319768c537df811532d2a6776597
B. Smreczak <b.smreczak@gmail.com>
2024-08-09 23:26:12 +0200

    First solution, basic tasks solved

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,57 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int INITIAL_SIZE = 10;
+    private int currentSize = INITIAL_SIZE;
+    private int size = 0;
+    private T[] table = (T[]) new Object[currentSize];
+
+    public void push(T value) {
+        if (size >= currentSize) {
+            table = grow();
+        }
+        table[size] = value;
+        size++;
     }
 
     public T peek() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        return table[size - 1];
     }
 
     public T pop() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        T value = peek();
+        table[size - 1] = null;
+        size--;
+        return value;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private T[] grow() {
+        double newLength = table.length * 1.5;
+        currentSize = (int) newLength;
+        T[] newTable = (T[]) new Object[currentSize];
+        for (int i = 0; i < table.length; i++) {
+            newTable[i] = table[i];
+        }
+        return newTable;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 }
+
+
+
+
