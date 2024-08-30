3ebc3a243dfc09fe14b54569f169d2ce56516294
Oleksii <obabych1@stu.vistula.edu.pl>
2024-08-30 14:20:41 +0200

    commit task

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,55 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elementData;
+    private int size;
+
+    public MateStack() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void push(T value) {
+        ensureCapacity();
+        elementData[size] = value;
+        size++;
+    }
+
+    private void ensureCapacity() {
+        if (size == elementData.length) {
+            int newCapacity = elementData.length * 2;
+            Object[] newArray = new Object[newCapacity];
+            System.arraycopy(elementData, 0, newArray, 0, elementData.length);
+            elementData = newArray;
+        }
     }
 
     public T peek() {
-        return null;
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        return (T) elementData[size - 1];
+    }
+
+    private boolean isEmpty() {
+        return size == 0;
     }
 
     public T pop() {
-        return null;
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        size--;
+        T element = (T) elementData[size];
+        elementData[size] = null;
+        return element;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 }
