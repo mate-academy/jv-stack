6d11d8dab847bd60ffc8a37ddcbb5f748e20cc95
Damian Frynas <cz.damax@gmail.com>
2024-10-16 14:49:17 +0200

    Created nesesery implementation all tests passed

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,53 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private T[] elements;
+    private int size;
+    private static final int INITIAL_CAPACITY = 10;
+
+    @SuppressWarnings("unchecked")
+    public MateStack() {
+        elements = (T[]) new Object[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    public void push(T value) {
+        if (size == elements.length) {
+            resize();
+        }
+        elements[size++] = value;
     }
 
     public T peek() {
-        return null;
+        throwExeption();
+        return elements[size - 1];
     }
 
     public T pop() {
-        return null;
+        throwExeption();
+        T value = elements[--size];
+        elements[size] = null;
+        return value;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
+
+    @SuppressWarnings("unchecked")
+    private void resize() {
+        T[] newArray = (T[]) new Object[elements.length * 2];
+        System.arraycopy(elements, 0, newArray, 0, elements.length);
+        elements = newArray;
+    }
+
+    private void throwExeption() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+    }
+
+
 }
