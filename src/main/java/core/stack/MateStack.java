643c173d42715c430baaa267c0d6728f508c437f
B4rt1omiej <bartlomiej.karol.gacek@gmail.com>
2024-10-17 16:41:08 +0200

    Bugged solution of the task

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,49 @@
+
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.Arrays;
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int DEAFULT_SIZE = 8;
+    private static Object[] stack;
+    private static int size = 0;
+
+    public MateStack() {
+        stack = new Object[DEAFULT_SIZE];
+        Arrays.fill(stack, null);
+    }
+
+    public void push(T value) throws RuntimeException {
+        if (size <= stack.length) {
+            stack[size] = value;
+            size++;
+        } else {
+            throw new StackOverflowError();
+        }
     }
 
     public T peek() {
-        return null;
+        if (size > 0) {
+            return (T) stack[size - 1];
+        } else {
+            throw new EmptyStackException();
+        }
     }
 
     public T pop() {
-        return null;
+        if (size > 0) {
+            Object value = stack[size - 1];
+            stack[size] = null;
+            size = size - 1;
+            return (T) value;
+        } else {
+            throw new EmptyStackException();
+        }
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 }
+
