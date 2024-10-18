2f9c565b82e86fd8dfe552e50247c24d2fa9ecc8
B4rt1omiej <bartlomiej.karol.gacek@gmail.com>
2024-10-18 16:20:49 +0200

    Solution with changed Object[] to T[] and with better grow method.

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -4,21 +4,18 @@ import java.util.EmptyStackException;
 
 public class MateStack<T> {
     private static final int DEFAULT_SIZE = 8;
-    private Object[] stack;
-    //private T[] stack;
-    //Type parameter 'T' cannot be instantiated directly
+    private T[] stack;
     private int size = 0;
-    private final int multiplier = 2;
+    private final int MULTIPLIER = 2;
 
     public MateStack() {
-        stack = new Object[DEFAULT_SIZE];
+        this.stack = (T[]) new Object[DEFAULT_SIZE];
     }
 
-    public void grow() {
-        if (size < stack.length) {
-            int newCapacity = size * multiplier;
-            System.arraycopy(stack, 0, stack = new Object[newCapacity], 0, size);
-        }
+    private void grow() {
+        T[] newArray = (T[]) new Object[stack.length * MULTIPLIER];
+        System.arraycopy(stack, 0, newArray, 0, stack.length);
+        stack = newArray;
     }
 
     public void push(T value) {
