940253f3e38a18fcdd5c3d3869b091604b80e248
B4rt1omiej <bartlomiej.karol.gacek@gmail.com>
2024-10-18 14:09:08 +0200

    Corrected solution of the task, with removed ArrayFill, with added grow method and optymalized few thinks. I tryed change Object[] to T[] but it pop a problem that i wasn't sure how to fix, thats why i remain Object instead of T.

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,17 +1,24 @@
-
 package core.stack;
 
-import java.util.Arrays;
 import java.util.EmptyStackException;
 
 public class MateStack<T> {
     private static final int DEFAULT_SIZE = 8;
     private Object[] stack;
+    //private T[] stack;
+    //Type parameter 'T' cannot be instantiated directly
     private int size = 0;
+    private final int multiplier = 2;
 
     public MateStack() {
         stack = new Object[DEFAULT_SIZE];
-        Arrays.fill(stack, null);
+    }
+
+    public void grow() {
+        if (size < stack.length) {
+            int newCapacity = size * multiplier;
+            System.arraycopy(stack, 0, stack = new Object[newCapacity], 0, size);
+        }
     }
 
     public void push(T value) {
@@ -19,7 +26,7 @@ public class MateStack<T> {
             stack[size] = value;
             size++;
         } else {
-            throw new StackOverflowError();
+            grow();
         }
     }
 
@@ -34,8 +41,7 @@ public class MateStack<T> {
     public T pop() {
         if (size > 0) {
             Object value = stack[size - 1];
-            stack[size - 1] = null;
-            size = size - 1;
+            size--;
             return (T) value;
         } else {
             throw new EmptyStackException();
