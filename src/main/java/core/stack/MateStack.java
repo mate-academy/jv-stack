ea85d9eeb95ed7394770679ce7288298beec4735
Weronika Rzekiecka <weronikarzekiecka@interia.pl>
2025-03-18 12:34:36 +0100

    solution2

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,64 +1,55 @@
 package core.stack;
 
-import java.util.Arrays;
 import java.util.EmptyStackException;
 
 public class MateStack<T> {
-    private static final int DEFAULT_CAPACITY = 8;
-    private static final int GROW = 2;
+    private Node<T> top;
     private int size;
-    private Object[] array;
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 
     public MateStack() {
-        array = new Object[DEFAULT_CAPACITY];
-    }
-
-    @SuppressWarnings("unchecked")
-    public T pop() {
-        if (size > 0) {
-            size--;
-            T removedElement = (T) array[size];
-            array[size] = null;
-            return removedElement;
-        } else {
-            throw new EmptyStackException();
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    public T peek() {
-        if (size > 0) {
-            return (T) array[size - 1];
-        } else {
-            throw new EmptyStackException();
-        }
+        top = null;
+        size = 0;
     }
 
     public void push(T value) {
-        if (size == array.length) {
-            ensureCapacity();
-        }
-        array[size] = value;
+        Node<T> newNode = new Node<>(value);
+        newNode.next = top;
+        top = newNode;
         size++;
     }
 
-    @Override
-    public String toString() {
-        return Arrays.toString(array);
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        T value = top.data;
+        top = top.next;
+        size--;
+        return value;
+    }
 
+    public T peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return top.data;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 
     public int size() {
         return size;
-    }
-
-    @SuppressWarnings("unchecked")
-    private void ensureCapacity() {
-        int newCapacity = array.length * GROW;
-        T[] newArray = (T[]) new Object[newCapacity];
-        for (int i = 0; i < array.length; i++) {
-            newArray[i] = (T) array[i];
-        }
-        array = newArray;
     }
 }
