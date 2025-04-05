34fe23ed78e33428e40fb5bb6466c4243062492e
Marika Żaboklicka <marika.zaboklicka@wp.pl>
2025-04-05 19:18:36 +0200

    format updates

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -3,12 +3,11 @@ package core.stack;
 import java.util.EmptyStackException;
 
 public class MateStack<T> {
-
     private Node<T> head;
     private int size = 0;
 
     public void push(T value) {
-        Node<T> node = new Node<>(value, head);
+        Node<T> node = new Node<T>(value, head);
         head = node;
         size++;
     }
@@ -36,8 +35,8 @@ public class MateStack<T> {
     }
 
     private static class Node<T> {
-       private T value;
-       private Node<T> prev;
+        private T value;
+        private Node<T> prev;
 
         public Node(T value, Node<T> prev) {
             this.value = value;
