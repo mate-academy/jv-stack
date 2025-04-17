5009c4b17c0a5ee20fd9cf9eff74753a1ba19f6d
Jacek Kubiak <jacek.ku@poczta.fm>
2025-04-17 09:48:15 +0200

    Tack to check

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,47 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.Arrays;
+import java.util.EmptyStackException;
 
+public class MateStack<T> {
+    private static final int DEFAULT_SIZE = 8;
+    private Object[] stack;
+    private int size = 0;
+
+    public MateStack() {
+        stack = new Object[DEFAULT_SIZE];
+        Arrays.fill(stack, null);
+        }
+
+    public void push(T value) {
+        if (size < stack.length) {
+            stack[size] = value;
+            size++;
+        } else {
+            throw new StackOverflowError();
+        }
     }
 
     public T peek() {
-        return null;
+        if (size > 0) {
+            return (T) stack[size - 1];
+        } else {
+            throw new EmptyStackException();
+        }
     }
 
     public T pop() {
-        return null;
+        if (size > 0) {
+            Object value = stack[size - 1];
+            stack[size - 1] = null;
+            size = size - 1;
+            return (T) value;
+        } else {
+            throw new EmptyStackException();
+        }
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 }
